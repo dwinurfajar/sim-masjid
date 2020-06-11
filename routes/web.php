e886b9e043e56ea1589246a3597f7b3a28e05c67
dynatic <dwinurfajar05@gmail.com>
2020-06-11 13:53:46 +0700

    /media/leaf/DATA1/web/sim-masjid

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin', 'middleware'=>['admin','auth']], function(){
+	Route::resource('', 'UserController');
+});
