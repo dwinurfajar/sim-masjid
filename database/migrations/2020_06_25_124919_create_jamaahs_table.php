ed03743db371e35d1b460bfefec2a2323fdc5015
dynatic <dwinurfajar05@gmail.com>
2020-06-27 11:22:59 +0700

    .

--- database/migrations/2020_06_25_124919_create_jamaahs_table.php
+++ database/migrations/2020_06_25_124919_create_jamaahs_table.php
@@ -16,11 +16,11 @@ class CreateJamaahsTable extends Migration
         Schema::create('jamaahs', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->boolean('jenisKelamin');
+            $table->char('jenisKelamin', 1);
             $table->string('telephone');
             $table->string('alamat');
             $table->char('aktif', 1)->default('0');
-            $table->boolean('zakat')->default('0');
+            $table->char('zakat', 1)->default('0');
             $table->string('latt');
             $table->string('long');
             $table->timestamps();
