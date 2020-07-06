39223cd2f3a92d46ae7029c3bf6df0a5a83ccdf9
dynatic <dwinurfajar05@gmail.com>
2020-07-06 13:03:14 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'FrontController@index');
+Route::get('/', 'HomeController@index');
 /*
 Route::get('/', function(){
 	return view('welcome');
