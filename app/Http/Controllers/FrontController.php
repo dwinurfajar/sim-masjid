c4c4f309073f137fb6812fe49f555f3ca0070123
dynatic <dwinurfajar05@gmail.com>
2020-07-06 12:58:31 +0700

    .

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('guest');
+    }
     public function index(){
         $jamaah = DB::table('jamaahs')->get();
 
